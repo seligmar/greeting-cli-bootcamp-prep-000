7b28025815024befcd7dff8a67a40d8d82567e3a
Mary Selig <seligmar@gmail.com>
2019-04-29 18:42:48 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -2,6 +2,10 @@
 
 def greeting(name)
   
+<<<<<<< HEAD
  puts "Hello #{name.strip}. It's nice to meet you."
+=======
+ puts.strip "Hello #{name}. It's nice to meet you."
+>>>>>>> 99728f7c1831f32c823bf41757fc61dc00162961
  
 end 
